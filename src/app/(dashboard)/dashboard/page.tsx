84cbbe66a366a2c29afc9d46a4b4776d320b4bea
habibur-pro <va.habibur@gmail.com>
2024-07-01 09:14:47 +0600

    initial setup

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>da</div>;
+};
+
+export default page;
---
+++ src/app/(home)/login/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return (
+    <div>
+      <Button>Check</Button>
+    </div>
+  );
+};
+
+export default page;
